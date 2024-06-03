48216ba5957a55856cf74af4e153558740de0b9f
NAVS <ronnel.navarro2020@gmail.com>
2024-06-03 20:42:14 +0800

    disabled git ignore

---
+++ .config/dbconnect.php
@@ -0,0 +1,35 @@
+<?php
+
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'booknearn';
+
+    $con = mysqli_connect($server, $username, $password, $dbname);
+
+    if (!$con) {
+        echo "Database Connection Error!";
+    }
+
+    session_start();
+
+    include_once 'function.php';
+    
+    $token = $_SESSION['token'];
+
+    $sql_session = mysqli_query($con, "SELECT `fullname` FROM `users` WHERE `_token` = '$token'");
+    if (mysqli_num_rows($sql_session) < 1)
+    {
+        $sql_session = mysqli_query($con, "SELECT `fullname` FROM `admin` WHERE `_token` = '$token'");
+        $admin = "TRUE";
+    }
+    $session = mysqli_fetch_array($sql_session);
+
+    $name = $session['fullname'];
+    $img = $session['img'];
+    if ($img == '') {
+        $img = 'default.png';
+    }
+
+    $date1 = date('Y').'-'.date('m').'-01';
+    $date2 = date('Y-m-d');
